3c4a4ed5800ecb3425e4b4440ed71cd074572d1b
s0172067 <113241344+s0172067@users.noreply.github.com>
2022-11-22 09:56:27 +0300

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -3,17 +3,17 @@ function click1() {
     let f1 = document.getElementById("field1");
     let f2 = document.getElementById("field2");
     let r = document.getElementById("result");
-    let F1 = Number(f1.value);
-    let F2 = Number(f2.value);
+    let F1 = Math.abs(Number(f1.value));
+    let F2 = Math.abs(Number(f2.value));
     let q;
-   // if (F1 % 10 >= "0" && F1 % 10 <= "9" && F2 % 10 <= "9" && F2%10 >= "0") {
-        if (f1.value >= 0 && f2.value >= 0) {
-            r.textContent = Number(f1.value) * Number(f2.value);
-        }
-        else r.textContent = " Not right !!! ";
-  //  } else r.textContent ="Not right !!! ";
-    
-    
+     if (F1 % 10 >= "0" && F1 % 10 <= "9" && F2 % 10 <= "9" && F2%10 >= "0") {
+    if (f1.value >= 0 && f2.value >= 0) {
+        r.textContent = Number(f1.value) * Number(f2.value);
+    }
+    else r.textContent = " Not right ( < 0 ) !!! ";
+      } else r.textContent ="Not right (  need numbers !) !!!!!! ";
+
+
     //r.textContent = q;
 }
 let buttom = document.getElementById("button1");
